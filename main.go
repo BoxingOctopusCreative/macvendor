7d13f056b7538f2d75a073c409510582f740b027
Ryan Draga <ryan.draga@tuxotaku.com>
2017-12-09 22:29:38 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// MacAddr Address Flag
+var MacAddr = flag.String("mac", "", "MAC Address")
+
+// Usage help message
+var Usage = func() {
+	fmt.Println("Get Vendor for specified MAC Address \n Usage:")
+	flag.PrintDefaults()
+}
+
+func getMac() {
+
+	mvAPI := "https://api.macvendors.com/" + *MacAddr
+	resp, _ := http.Get(mvAPI)
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		defer resp.Body.Close()
+		fmt.Printf("%s", err)
+	} else {
+		defer resp.Body.Close()
+		fmt.Printf("%s", body)
+	}
+}
+
+func main() {
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		Usage()
+	} else {
+		getMac()
+	}
+}

1617fafebedfd04064baca14c87ede9753a2a636
Ryan Draga <ryan.draga@scotiabank.com>
2018-01-03 11:31:25 -0500

    update homebrew formula

--- macvendor.rb
+++ macvendor.rb
@@ -1,21 +1,15 @@
-Class Macvendor < Formula
+class Macvendor < Formula
+  desc "CLI tool which looks up a given MAC Address on macvendors.com"
+  homepage "https://github.com/tuxotaku/macvendor"
+  url "https://github.com/TuxOtaku/macvendor/releases/download/1.0/macvendor-darwin-amd64"
+  sha256 "cb67c08ece570cdac599d87ad295e46b1a195213ecf18ad986b584e671cbf2c2"
 
-    desc "CLI tool which looks up hardware vendor names for network devices based on the macvendors.com API"
-    homepage "https://github.com/tuxotaku/macvendor"
-    url "https://github.com/TuxOtaku/macvendor/releases/download/1.0/macvendor-darwin-amd64"
-    sha256 "cb67c08ece570cdac599d87ad295e46b1a195213ecf18ad986b584e671cbf2c2"
+  def install
+    bin.install "macvendor-darwin-amd64"
+    system "mv #{bin}/macvendor-darwin-amd64 #{bin}/macvendor"
+  end
 
-    bottle do
-        cellar :any_skip_relocation
-        sha256 "cb67c08ece570cdac599d87ad295e46b1a195213ecf18ad986b584e671cbf2c2" => :sierra
-    end
-
-    def install
-        bin.install "macvendor"
-    end
-
-    test do
-        shell_output("#{bin}/macvendor -h", 1)
-    end
-
+  test do
+    shell_output("#{bin}/macvendor -h", 1)
+  end
 end
